17597afb23f6a096195d69da61bd8b96539ef611
talya toledano <talyatoledano10@gmail.com>
2026-07-12 21:28:10 +0300

    fix: move authInterceptor.ts to src/app/

--- project-claient/src/app/app.config.ts
+++ project-claient/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from '../authInterceptor';
+import { authInterceptor } from './authInterceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
